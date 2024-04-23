6d9f80d7a97532f1aaaa1d40abc85cfc31b97f95
Andrey Mir <59264372+ALFARD777@users.noreply.github.com>
2024-04-23 17:52:02 +0300

    Добавьте файлы проекта.

---
+++ TodoList.Domain/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoList.Domain.Entities
+{
+    public abstract class Entity<IPrimaryKey>
+    {
+        public IPrimaryKey Id { get; set; }
+    }
+    public class Entity : Entity<int>
+    {
+
+
+    }
+}
